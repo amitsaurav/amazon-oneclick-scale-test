a1ad82be56d9c89e4ec5b46f59108ddf1c853d2c
Amit Saurav <amitsaurav@gmail.com>
2015-05-28 07:15:14 -0700

    Formatting changes.

--- parallel.js
+++ parallel.js
@@ -2,8 +2,8 @@ var exec = require("child_process").exec;
 
 // I use --no-colors because I redirect all output from this script to a file
 for (var i=0; i<50; i++) {
-	var process = exec('casperjs test purchase-scale.js --pid=' + i + ' --no-colors');
-	process.stdout.on('data', function(data){
-		console.log(data);
-	});
+    var process = exec('casperjs test purchase-scale.js --pid=' + i + ' --no-colors');
+    process.stdout.on('data', function(data){
+        console.log(data);
+    });
 }
--- purchase-scale.js
+++ purchase-scale.js
@@ -3,8 +3,8 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
     casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
 
     casper.on('remote.message', function(msg) {
-    	this.echo('[process ' + casper.cli.get("pid") + '] remote message: ' + msg);
-	});
+        this.echo('[process ' + casper.cli.get("pid") + '] remote message: ' + msg);
+    });
 
     casper.echo('[process ' + casper.cli.get("pid") + '] Starting request at: ' + new Date().toLocaleString());
 
@@ -32,7 +32,7 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
         });
     casper.waitForSelector("form[name=signIn] input[type=submit]",
         function success() {
-        	this.echo('[process ' + casper.cli.get("pid") + '] Logging-in');
+            this.echo('[process ' + casper.cli.get("pid") + '] Logging-in');
             this.click("form[name=signIn] input[type=submit]");
         },
         function fail() {
@@ -42,7 +42,7 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
     // Turning ON 1-click for this browser
     casper.waitForSelector("form input[type=submit][value='Turn on']",
         function success() {
-        	this.echo('[process ' + casper.cli.get("pid") + '] Turning On 1-click');
+            this.echo('[process ' + casper.cli.get("pid") + '] Turning On 1-click');
             this.click("form input[type=submit][value='Turn on']");
         },
         function fail() {
@@ -50,8 +50,8 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
         });
     casper.waitForSelector("form input[type=submit][value='Turn off']",
         function success() {
-        	test.assertExists("form input[type=submit][value='Turn off']");
-        	this.echo('[process ' + casper.cli.get("pid") + '] 1-Click is turned ON.');
+            test.assertExists("form input[type=submit][value='Turn off']");
+            this.echo('[process ' + casper.cli.get("pid") + '] 1-Click is turned ON.');
         },
         function fail() {
             test.assertExists("form input[type=submit][value='Turn off']");
@@ -59,7 +59,7 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
 
     // Navigating to an ASIN's offerlisting page to test a specific offer, not necessarily the winning offer
     casper.thenOpen("http://www.amazon.com/gp/offer-listing/B0016OTD1G/", function() {
-    	this.echo('[process ' + casper.cli.get("pid") + '] Opened OL page');
+        this.echo('[process ' + casper.cli.get("pid") + '] Opened OL page');
     });
 
     // This is only required if a specific offerlisting id needs to be used for testing, which is not
@@ -73,12 +73,12 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
                     if (typeof element.name === "undefined") continue;
 
                     if (element.name.indexOf('offering') == 0) {
-                    	console.log("Setting Custom OfferId");
+                        console.log("Setting Custom OfferId");
                         element.name = '<Set the long offeringId>';
                     }
 
                     if (element.name.indexOf('merchant-id') == 0) {
-                    	console.log("Setting Custom MerchantId");
+                        console.log("Setting Custom MerchantId");
                         element.value = '<Set the merchant id of the customer offeringId>';
                     }
                 }
@@ -89,27 +89,27 @@ casper.test.begin('Purchase Scalability Tester', 2, function(test) {
         });
 
     // All the processes need to wait on a specific minute to hit the buy-box at the same instant
-	casper.then(function() {
-		casper.echo('[process ' + casper.cli.get("pid") + '] Synchronizing purchase with all processes...');
-		casper.waitFor(function check() {
-		    return new Date().getMinutes() === <Absolute minute to wait until>;
-		}, function then() {
-		    casper.echo('[process ' + casper.cli.get("pid") + '] Clicking buy at: ' + new Date().toLocaleString());
-		    this.click("form[action='/gp/legacy-handle-buy-box.html/ref=olp_atc_new_1'] input[type='submit']");
-		}, function timeout() {
-			casper.echo('[process ' + casper.cli.get("pid") + '] Timed-out!');
-		}, 300000);
-	});
+    casper.then(function() {
+        casper.echo('[process ' + casper.cli.get("pid") + '] Synchronizing purchase with all processes...');
+        casper.waitFor(function check() {
+            return new Date().getMinutes() === <Absolute minute to wait until>;
+        }, function then() {
+            casper.echo('[process ' + casper.cli.get("pid") + '] Clicking buy at: ' + new Date().toLocaleString());
+            this.click("form[action='/gp/legacy-handle-buy-box.html/ref=olp_atc_new_1'] input[type='submit']");
+        }, function timeout() {
+            casper.echo('[process ' + casper.cli.get("pid") + '] Timed-out!');
+        }, 300000);
+    });
 
     // If order number is present, the order succeeded
-	casper.waitForSelector("h5",
-		function success() {
-			test.assertExists(x('//h5[contains(text(),"Order Number:")]'));
-			casper.echo('[process ' + casper.cli.get("pid") + '] Successfully placed order!')
-		},
-		function fail() {
-			test.assertExists(x('//h5[contains(text(),"Order Number:")]'), '[process ' + casper.cli.get("pid") + '] Failed placing order!');
-		});
+    casper.waitForSelector("h5",
+        function success() {
+            test.assertExists(x('//h5[contains(text(),"Order Number:")]'));
+            casper.echo('[process ' + casper.cli.get("pid") + '] Successfully placed order!')
+        },
+        function fail() {
+            test.assertExists(x('//h5[contains(text(),"Order Number:")]'), '[process ' + casper.cli.get("pid") + '] Failed placing order!');
+        });
 
     // Take the screenshot of the final page before completing
     casper.run(function() {
